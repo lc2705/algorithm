b50ea677ba1e55afd0d5138cfca12a00520d1b30
lc2705 <liuchang2705@163.com>
2015-10-10 22:54:10 +0800

    1010

--- queen.cpp
+++ queen.cpp
@@ -4,6 +4,7 @@ using namespace std;
 void SetQueen(int *a,int cur,int n);
 void Print(int *a,int n);
 int vis[3][600] = {0};
+bool finded;
 
 int main()
 {
@@ -13,6 +14,7 @@ int main()
 	{
 		cin >> n;
 		if(n == 0) break;
+		finded = false;
 		SetQueen(a,0,n);
 	}
 	return 0;	
@@ -20,8 +22,12 @@ int main()
 
 void SetQueen(int *a, int cur, int n)
 {
+	if(finded)
+		return;
+		
 	if(cur == n)
 	{
+		finded = true;
 		Print(a, n);
 		return;
 	}
@@ -54,3 +60,70 @@ void Print(int *a, int n)
 		cout << a[i] + 1 << ' ';
 	cout << endl;
 }
+
+#if 0
+
+/*传统DFS回溯 TLE (n超过30就难以搜索)*/
+
+typedef class
+{
+    public:
+        int r,c;
+}location;
+
+location pos[301];  //标记已填放的皇后坐标
+
+bool col[301];  //列标记
+int n;  //皇后数
+
+bool DFS(int x,int p)
+{
+    if(p==n)
+        return true;
+
+    bool sign;
+    for(int y=1;y<=n;y++)  //枚举当前行x的每一列
+    {
+        bool flag=false;
+        for(int i=1;i<=p;i++)  //枚举每一个已填放的皇后
+            if(abs(x-pos[i].r) == abs(y-pos[i].c))   //检查当前准备填放皇后的位置(x,y)是否在他们的斜边上
+            {        //其实就是标记斜率，斜率等于1或-1的格不允许填放皇后
+                flag=true;
+                break;
+            }
+            
+        if(!flag && !col[y])
+        {
+            col[y]=true;
+            pos[p+1].r=x;
+            pos[p+1].c=y;
+            sign=DFS(x+1,p+1);
+
+            if(sign)
+                return true;
+            else
+                col[y]=false; //回溯,pos[]的记录会自动被新值覆盖，所以不用特意处理
+        }
+    }
+    return false;  //当前行x的所有列y均不允许放皇后，说明前一行填错了
+}
+
+int main(void)
+{
+    while(cin>>n)
+    {
+        if(!n)
+            break;
+
+        int p=0; //pos[]指针,已填充的棋子数
+        memset(col,false,sizeof(col));
+
+        DFS(1,p);
+
+        for(int i=1;i<=n;i++)
+            cout<<pos[i].c<<' ';
+        cout<<endl;
+    }
+    return 0;
+}
+#endif
--- stick.cpp
+++ stick.cpp
@@ -2,14 +2,85 @@
 #include <algorithm>
 using namespace std;
 
-bool FindLength();
+bool selected[64];
+int stick[64];
+
+bool cmp(int a, int b);
+int InitStick(int n);
+bool FindLength(int initlen,int len,int cur, int n);
 
 int main()
 {
+	int n;
+	while(1)
+	{
+		cin >> n;
+		if(n == 0)
+	 		break;
+	 		
+		int i;
+		int sum = InitStick(n);
+		sort(stick,stick + n,cmp);
+		for(i = stick[0]; i < sum ; i++)
+		{
+			if(!(sum % i) && FindLength(0,i,0,n))
+				break;
+		}
+		cout << i << endl;
+	}
 	return 0;
 }
 
-bool FindLength()
+bool FindLength(int initlen, int len, int cur, int n)
 {
+	if(cur == n)
+	{
+		if(initlen == 0)
+			return true;
+		else 
+			return false;
+	}
+
+	int failstick = 0;	
+	for(int i = 0; i < n ; i++)
+	{
+		if(selected[i] || stick[i] == failstick)
+			continue;
 		
+		selected[i] = true;
+		if(initlen + stick[i] < len)
+		{
+			if(FindLength(initlen + stick[i], len, cur + 1, n))
+				return true;
+		}
+		else if(initlen + stick[i] == len)
+		{
+			if(FindLength(0, len, cur + 1, n))
+				return true;
+		}
+		failstick = stick[i];
+		selected[i] = false;
+		
+		if(initlen == 0) //important!!!
+			break;
+	}
+	
+	return false;	
+}
+
+int InitStick(int n)
+{
+	int sum = 0;
+	for(int i = 0; i < n; i++)	
+	{
+		cin >> stick[i];
+		sum += stick[i];
+		selected[i] = false;
+	}
+	return sum;
+}
+
+bool cmp(int a,int b)
+{
+	return a > b;
 }
