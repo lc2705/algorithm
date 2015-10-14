12e1c0d29e1812ecd264c6f8e1e0de889b54aeac
lc2705 <liuchang2705@163.com>
2015-10-14 23:28:51 +0800

    1014

--- greedy/wall.cpp
+++ greedy/wall.cpp
@@ -1,52 +1,80 @@
 #include <iostream>
-#include <alorithm>
+#include <cstdio>
+#include <cstring>
+#include <set>
 using namespace std;
 
+#define WIDTH 100
 struct wall
 {
-	int beg,end;
+	int beg;
+	int end;
+};
+
+struct cmp
+{
+	bool operator()(const wall &a, const wall &b)const
+	{
+		if(a.end == b.end)
+			return a.beg < b.beg;
+		else
+			return a.end > b.end;
+	}
 };
 
 int main()
 {
 	int T,n,k;
-	wall w[110];
-	int wn[100];
-	int i,j;
+	multiset<wall,cmp> wset;
+	int wn[WIDTH];
 	cin >> T;
 	while(T--)
 	{
-		int tmp;
+		wall tmp;
+		int i,j,val;
+		int cnt = 0;
+		memset(wn,0,sizeof(int) * 100);
+		
 		cin >> n >> k;
 		for(i = 0; i < n; i++)
 		{
-			cin >> w[i].beg >> tmp >> w[i].end >> tmp;
-			if(w[i].end < w[i].beg)
+			cin >> tmp.beg >> val >> tmp.end >> val;
+			if(tmp.beg > tmp.end)
 			{
-				tmp = w[i].end;
-				w[i].end = w[i].beg;
-				w[i].beg = tmp;
+				val = tmp.beg;
+				tmp.beg = tmp.end;
+				tmp.end = val;
 			}
-		}	
-		
-		memset(wn, 0, sizeof(int) * 100);
-		//sort(w, w + n, cmp);
-		for(i = 0; i < n; i++)
-			for(j = w[i].beg; j <= w[i].end; j++)
+			wset.insert(tmp);
+			for(j = tmp.beg; j <= tmp.end; j++)
 				wn[j]++;
-#ifdef BUGDE
-		for(i = 0; i < 100; i++)
-			cout << wn[i] << endl;
-#endif
-		int a = 0,b = 0;
-		for(i = 0; i < 100; i++)
-		{
-			if(wn[i] > k)
-			{
-				a = i;
-				
-			} 
 		}
+		for(i = 0; i < WIDTH; i++)
+		{
+			cout << wn[i] << endl;
+		}
+		
+		for(i = 0; i < WIDTH; i++)
+		{
+			while(wn[i] > k)
+			{
+				multiset<wall,cmp>::iterator iter = wset.begin();
+				while(iter != wset.end())
+				{
+					if(iter->beg <= i)
+					{
+						cout << iter->beg << " " << iter->end << endl;
+						for(int s = iter->beg; s <= iter->end; s++)
+							wn[s]--;
+						wset.erase(iter);
+						cnt++;
+						break;
+					}
+					iter++;
+				}
+			}
+		}
+		printf("%d\n",cnt);
 	}
 	return 0;
 }
